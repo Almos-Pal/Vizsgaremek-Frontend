20ebb18d27bf024fdf41d06962e5bd9c9b5b7502
RobozPeter <peti.roboz@gmail.com>
2025-02-07 11:36:00 +0100

    fix(SCRUM-34): fix overflow

--- frontend/src/components/client/dashboard_items/TodaysWorkout/TodaysWorkout.tsx
+++ frontend/src/components/client/dashboard_items/TodaysWorkout/TodaysWorkout.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { list } from "postcss";
 import { Button } from "../../index";
 import { Text } from "@/components/server";
 import styles from './TodaysWorkout.module.scss';
@@ -18,7 +17,7 @@ function TodaysWorkout() {
     let list: WorkoutProps[] = [
         {
 
-            name: "Bench Press",
+            name: "zottman kukimuki mkikimuki ",
             sets: 3,
             reps: 10
         },
@@ -50,23 +49,23 @@ function TodaysWorkout() {
         console.log(human)
     }
     return (
-        <div id="mainDiv" className={clsx(styles.mainDiv, "sm:max-w-[500px] max-w-[325px] flex flex-wrap sm:flex-nowrap sm:flex-col sm:justify-normal justify-center  sm:m-2.5 sm:p-5 rounded-lg ")}>
+        <div id="mainDiv" className={clsx(styles.mainDiv, "sm:max-w-[600px] max-w-[325px] flex flex-wrap sm:flex-nowrap sm:flex-col sm:justify-normal justify-center  sm:m-2.5 sm:p-5 rounded-lg ")}>
             <div className="w-full max-w-[500px] flex-row mb-8">
                 <div className="max-h-[30px] text-center">
                     <Text variant="h4">Mai Edzés</Text>
                 </div>
             </div>
-            <div className="flex sm:flex-row flex-col justify-center  max-w-[500px]">
-                <div className="max-w-[300px]  hidden  sm:flex sm flex-wrap justify-center flex-row mr-4 p-0 ">
+            <div className="flex sm:flex-row flex-col justify-center">
+                <div className="max-w-[300px]  hidden  sm:flex sm:flex-wrap flex-nowrap justify-center flex-row mr-4 p-0 ">
                     <div className="mr-6">
                         {
                             list.slice(0, 5).map((item) => {
                                 return (
-                                    <div key={item.name} className=" max-h-[25px] flex flex-row justify-between gap-0 mb-5 pl-3 ">
-                                        <div className="flex flex-row min-w-[150px]  ">
+                                    <div key={item.name} className=" max-w-[400px] flex flex-row  justify-between gap-5 mb-5 pl-3 ">
+                                        <div className="flex flex-row   ">
                                             <Text variant="body-16">{item.name}</Text>
                                         </div>
-                                        <div className="flex flex-row min-w-[105px]  ">
+                                        <div className="flex flex-row   ">
                                             <div className=" w-full text-right">
                                                 <Text variant="body-16" >{item.sets}x{item.reps}</Text>
                                             </div>
@@ -88,17 +87,17 @@ function TodaysWorkout() {
                         </div>
                     }
                 </div>
-                <div className="max-w-[300px] min-w-[300px] sm:hidden visible w-full flex flex-row justify-center mr-4 p-0 ">
+                <div className="max-w-[300px] min-w-[300px] sm:hidden visible w-full flex flex-row justify-center mr-4 mb-3 p-0 ">
                     <div className="mr-6 flex flex-col gap-5">
                         {
                             list.slice(0, 3).map((item) => {
                                 return (
-                                    <div key={item.name} className=" max-w-[300px] flex flex-row justify-between  gap-10 ">
+                                    <div key={item.name} className=" max-w-[300px] flex flex-row justify-between  gap-10 pl-5 ">
                                         <div className="content-start">
                                             <Text variant="body-16">{item.name}</Text>
                                         </div>
-                                        <div className="justify-end">
-                                            <Text variant="body-16" >{item.sets}x{item.reps}</Text>
+                                        <div className="justify-end pl-5">
+                                            <Text  variant="body-16" >{item.sets}x{item.reps}</Text>
                                         </div >
                                     </div>
 
@@ -123,7 +122,7 @@ function TodaysWorkout() {
                         </div>
                     }
                 </div>
-                <div className={clsx(styles.humanDiv, "max-w-[300px] min-w-[200px] w-full sm:grid grid-rows-5 grid-cols-12 hidden justify-center sm:p-0 sm:m-0   rounded-lg")}>
+                <div className={clsx(styles.humanDiv, "sm:max-w-[200px] max-w-[325px] w-full sm:grid grid-rows-5 grid-cols-12 hidden justify-center sm:p-0 sm:m-0   rounded-lg")}>
                     <div className={clsx(styles.leftbuttonDesk, "justify-self-end")}>
                         <Button onClick={switchHuman} color="secondary" width={"40px"} style={{ borderRadius: "50%", width: "30px", height: "40px", padding: "0" }} iconOnly leftIcon="ArrowLeftIcon"></Button>
                     </div>
@@ -139,7 +138,7 @@ function TodaysWorkout() {
                         <Button onClick={switchHuman} color="secondary" width={"40px"} style={{ borderRadius: "50%", width: "30px", height: "40px", padding: "0" }} iconOnly leftIcon="ArrowRightIcon"></Button>
                     </div>
                 </div>
-                <div className={clsx(styles.humanDiv, "min-h-[300px] sm:hidden grid grid-rows-5 grid-cols-12  ml-0 mb-3 rounded-lg mr-4 ")}>
+                <div className={clsx(styles.humanDiv, "max-w-[200px] sm:hidden grid grid-rows-5 grid-cols-12  ml-0 mb-3 rounded-lg mr-4 ")}>
                     <div className={clsx(styles.leftbutton, "justify-self-end")}>
                         <Button onClick={switchHuman} color="secondary" width={"40px"} style={{ borderRadius: "50%", width: "30px", height: "40px", padding: "0" }} iconOnly leftIcon="ArrowLeftIcon"></Button>
                     </div>
