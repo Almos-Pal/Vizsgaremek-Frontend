42b2cd8e455e485c482f85cc444f97c5a0af7a54
DemendiAkos <akosdemendi@gmail.com>
2025-02-10 09:43:49 +0100

    fix: difference between the validation lenght of passwords between frontend and backend

--- frontend/src/utils/Validations/registerSchema.ts
+++ frontend/src/utils/Validations/registerSchema.ts
@@ -4,7 +4,7 @@ const registerSchema = Yup.object().shape({
     email: Yup.string().email("Helytelen email formátum").required("Email megadása kötelező"),
     username: Yup.string().required("Felhasználónév megadása kötelező"),
     password: Yup.string().required("Jelszó megadása kötelező")
-        .min(6, "A jelszónak legalább 6 karakter hosszúnak kell lennie")
+        .min(8, "A jelszónak legalább 8 karakter hosszúnak kell lennie")
         .matches(/[A-Z]/, "A jelszónak tartalmaznia kell legalább egy nagybetűt")
         .matches(/[0-9]/, "A jelszónak tartalmaznia kell legalább egy számot")
         .matches(/[!@#$%^&*(),.?":{}|<>]/, "A jelszónak tartalmaznia kell legalább egy speciális karaktert"),
