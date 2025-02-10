f61ac80350dd07c50dadced5d3f484d1232a6c92
RobozPeter <peti.roboz@gmail.com>
2025-02-10 11:06:05 +0100

    fix(SCRUM-36):weekly total picture can no longer be clicked on desktop

--- frontend/src/components/client/dashboard_items/WeeklyTotal/WeeklyTotal.tsx
+++ frontend/src/components/client/dashboard_items/WeeklyTotal/WeeklyTotal.tsx
@@ -13,7 +13,10 @@ function WeeklyTotal() {
                     <Text variant="h4">Heti Összesítő</Text>
                 </div> 
             </div>
-                <div className={clsx(styles.humanDiv,"max-w-[300px] flex justify-center min-w-[130px] w-full sm:mb-3 mb-8 ml-0 rounded-lg")}> 
+                <div className={clsx(styles.humanDiv,"max-w-[300px] sm:flex hidden justify-center min-w-[130px] w-full sm:mb-3 mb-8 ml-0 rounded-lg")}> 
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8_PtzGb9XgU_eXXauI4a56O3yuB5wqCpD2IZyFeMLFOsCoYnB72WUHnu7N7jhDMdlmyc&usqp=CAU" alt="" />
+            </div>
+            <div className={clsx(styles.humanDiv,"max-w-[300px] sm:hidden flex justify-center min-w-[130px] w-full sm:mb-3 mb-8 ml-0 rounded-lg")}> 
                     <a href={"/gyakorlat"}>
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8_PtzGb9XgU_eXXauI4a56O3yuB5wqCpD2IZyFeMLFOsCoYnB72WUHnu7N7jhDMdlmyc&usqp=CAU" alt="" />
                     </a>
