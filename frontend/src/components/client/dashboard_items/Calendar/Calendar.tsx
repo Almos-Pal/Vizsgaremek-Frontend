bfbd556011775bb294f2f902fa664197b6ffdeea
RobozPeter <peti.roboz@gmail.com>
2025-02-11 11:19:14 +0100

    feat(SCRUM-36):Calendar routing done

--- frontend/src/components/client/dashboard_items/Calendar/Calendar.tsx
+++ frontend/src/components/client/dashboard_items/Calendar/Calendar.tsx
@@ -5,21 +5,19 @@ import { useEffect, useState } from "react";
 import styles from './Calendar.module.scss';
 import Calendar from "react-calendar";
 import CalendarContainer from "./CalendarStyling";
+import { useRouter } from "next/navigation";
 
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
-function exercise(value: ValuePiece) {
-    console.log(value);
-  
-    
-}
 
 function CalendarWidget() {
+    const router = useRouter();
 
+    function handleDayClick(value: Date) {
+        router.push(`/edzes/${value.toISOString().split('T')[0]}`);
+    }
 
-
-    
     const [value, onChange] = useState<Value>(new Date());
     const [isClient, setIsClient] = useState(false);
     useEffect(() => {
@@ -29,10 +27,9 @@ function CalendarWidget() {
     return (
         <div id="mainDiv" className={clsx(styles.mainDiv, "sm:max-w-[360px]  max-w-[325px] w-full flex flex-col m-2.5 p-0 rounded-lg ")}>            
                 <CalendarContainer>
-                 {isClient ? (<Calendar onChange={onChange} onClickDay={exercise.bind(value)} value={value} />) : (<p>Loading</p>)}
+                 {isClient ? (<Calendar onChange={onChange}  onClickDay={handleDayClick} value={value} />) : (<p>Loading</p>)}
                  </CalendarContainer>
 
-                 <div className="overlay"></div>
         </div>
     )
 }
