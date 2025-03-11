34d1ea50d9ae78f8e6ccbfd7f50033ae547c2317
RobozPeter <peti.roboz@gmail.com>
2025-03-11 12:46:15 +0100

    feat(SCRUM-80) not found

--- frontend/src/app/edzes/page.tsx
+++ frontend/src/app/edzes/page.tsx
@@ -9,6 +9,7 @@ import { Button, Pagination } from '@/components/client';
 import { Modal } from '@/components/client/_modal';
 import { NewEdzesForm } from '@/components/client/_forms';
 import { useSession } from 'next-auth/react';
+import { Text } from '@/components/server';
 
 function EdzesekPage() {
     const { data: session } = useSession();
@@ -52,14 +53,33 @@ function EdzesekPage() {
         const storedStartTime = localStorage.getItem("edzesStartTime");
         storedStartTime ? localStorage.removeItem("edzesStartTime") : null;
     }
+    let header:string = 'Edzések';
+    if(edzesek?.items[0]){
+        let headerHelper = ""
+        edzesek?.items[0].gyakorlatok.map((gyakorlat: any) => {
+            if(gyakorlat.gyakorlat_id === gyakorlat_id){
+                headerHelper = gyakorlat.gyakorlat.gyakorlat_neve;
+            }
+        })
+        header = "Az alábbi edzések tartalmazzák a keresett gyakorlatot: " + help;
+    }
+    
 
     return (
-        <ContentLayout header="Edzések">
+        
+        
+            <ContentLayout header={header} >
+
             {workouts?.map((edzes: any) => (
                 <EdzesBlock key={edzes.edzes_id} edzes={edzes} />
             ))}
-           
+            {edzesek?.items.length === 0 && (
+            <Text className='justify-self-center' >Nincs találat</Text>
+          )}
+               
+
             <div className='flex justify-center mt-4 pb-4'>
+ 
                 <Button width={225} color='secondary' rightIcon='AddIcon' onClick={(handleNewEdzes)} >Edzés</Button>
             </div>
 
