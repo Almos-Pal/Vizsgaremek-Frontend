15e6cfb8ed9a1eb80a15e453cacebdd619746382
DemendiAkos <akosdemendi@gmail.com>
2025-03-12 10:10:23 +0100

    fix(SCRUM-79): Formik fields now remain with the same data after refresh and initialization

--- frontend/src/components/client/ProgressChart/ProgressChart.tsx
+++ frontend/src/components/client/ProgressChart/ProgressChart.tsx
@@ -1,18 +1,25 @@
-"use client"
+"use client";
 
 import useEdzes from "@/hooks/useEdzes";
 import { useSession } from "next-auth/react";
-import React, { useEffect, useState } from 'react';
-import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import React, { useEffect, useState } from "react";
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 import { Text } from "@/components/server";
 import dateParse from "@/utils/dateParse";
 import { useUserGyakorlat } from "@/hooks";
-import { UserGyakorlatGyakorlat } from "@/types";
-import styles from "./ProgressChart.module.scss";
+import { useRouter, useSearchParams } from "next/navigation";
 import { Formik, Form } from "formik";
 import { FormikSelect } from "../_inputs";
-import { useRouter, useSearchParams } from "next/navigation";
-import { date, number } from "yup";
+import styles from "./ProgressChart.module.scss";
 
 export default function ProgressChart() {
   interface gyakorlatProps {
@@ -27,66 +34,80 @@ export default function ProgressChart() {
 
   const { data: session } = useSession();
   const userId = session?.user.user_id;
-  const queryParams = new URLSearchParams();
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const { data: userGyakorlatData, isLoading: isloadingGyak } = useUserGyakorlat.fetchUserGyakorlatokAll({
-    userId: userId!,
-    page: 1,
-    limit: 1000
-  });
 
-  let gyakorlats: gyakorlatProps[] = [];
-  userGyakorlatData?.items.map((item) => {
-    gyakorlats.push({ gyakorlat_id: item.gyakorlat.gyakorlat_id, gyakorlat_neve: item.gyakorlat.gyakorlat_neve });
-  });
+  const { data: userGyakorlatData, isLoading: isLoadingGyak } =
+    useUserGyakorlat.fetchUserGyakorlatokAll({
+      userId: userId!,
+      page: 1,
+      limit: 1000,
+    });
+
+  const gyakorlats: gyakorlatProps[] =
+    userGyakorlatData?.items.map((item) => ({
+      gyakorlat_id: item.gyakorlat.gyakorlat_id,
+      gyakorlat_neve: item.gyakorlat.gyakorlat_neve,
+    })) || [];
 
   const gyakorlatsOptions = gyakorlats.map((gyakorlat) => ({
     value: gyakorlat.gyakorlat_id.toString(),
-    label: gyakorlat.gyakorlat_neve
+    label: gyakorlat.gyakorlat_neve,
   }));
 
-  const [selectedGyakorlat, setSelectedGyakorlat] = useState<number>(searchParams.get("gyakorlat_id") ? parseInt(searchParams.get("gyakorlat_id")!) : 0);
+
+  const initialGyakorlat =
+    searchParams.get("gyakorlat_id") || (gyakorlatsOptions[0]?.value || "");
+
+  const [selectedGyakorlat, setSelectedGyakorlat] = useState<number>(
+    parseInt(initialGyakorlat) || 0
+  );
   const [items, setItems] = useState<chartDataProps[]>([]);
 
-  const { data: tenDayData, isLoading: isLoadingUser, refetch, error } = useEdzes.getTenDayEdzesek(userId!, selectedGyakorlat);
+  const { data: tenDayData, refetch, error } = useEdzes.getTenDayEdzesek(
+    userId!,
+    selectedGyakorlat
+  );
 
 
   useEffect(() => {
     if (!searchParams.get("gyakorlat_id") && gyakorlatsOptions.length > 0) {
-      setSelectedGyakorlat(parseInt(gyakorlatsOptions[0].value));
-      router.push(`?gyakorlat_id=${gyakorlatsOptions[0].value}`, { scroll: false });
+      const firstGyakorlat = gyakorlatsOptions[0].value;
+      setSelectedGyakorlat(parseInt(firstGyakorlat));
+      router.push(`?gyakorlat_id=${firstGyakorlat}`, { scroll: false });
     }
   }, [gyakorlatsOptions, router]);
 
-
+ 
   useEffect(() => {
-    if (selectedGyakorlat !== null) {
+    if (selectedGyakorlat !== 0) {
       refetch().then((response) => {
-        let newItems: chartDataProps[] = [];
-        response.data && response.data.map((edzes) => {
-          let help: number = 0;
-          edzes.gyakorlatok.map((gyakorlat) => {
+        const newItems: chartDataProps[] = [];
+        response.data?.forEach((edzes) => {
+          let maxWeight = 0;
+          edzes.gyakorlatok.forEach((gyakorlat) => {
             if (gyakorlat.gyakorlat.gyakorlat_id === selectedGyakorlat) {
-              gyakorlat.szettek.map((set) => {
-                if (set.weight > help) {
-                  help = set.weight;
+              gyakorlat.szettek.forEach((set) => {
+                if (set.weight > maxWeight) {
+                  maxWeight = set.weight;
                 }
               });
 
-              newItems.push({ id: edzes.edzes_id, date: dateParse(new Date(edzes.datum)), weight: help });
-
+              newItems.push({
+                id: edzes.edzes_id,
+                date: dateParse(new Date(edzes.datum)),
+                weight: maxWeight,
+              });
             }
           });
         });
         setItems(newItems);
-        console.log(newItems);
       });
     }
   }, [selectedGyakorlat, refetch]);
 
-  if (isloadingGyak || !userId) {
+  if (isLoadingGyak || !userId) {
     return <Text>Loading...</Text>;
   }
 
@@ -97,69 +118,85 @@ export default function ProgressChart() {
       </div>
     );
   }
- 
+
   return (
     <div className={styles.container}>
-      <Text style={{ textAlign: "center" }} variant="h4">Fejlődési idővonal</Text>
+      <Text style={{ textAlign: "center" }} variant="h4">
+        Fejlődési idővonal
+      </Text>
       <div className={styles.main}>
-
         <Formik
-          initialValues={{ gyakorlat: "" }}
-          onSubmit={() => { }}
+          initialValues={{ gyakorlat: initialGyakorlat }}
+          enableReinitialize
+          onSubmit={() => {}}
         >
           {({ setFieldValue, values }) => {
             useEffect(() => {
               if (values.gyakorlat) {
-                const updatedParams = new URLSearchParams(window.location.search);
-                updatedParams.set("gyakorlat_id", values.gyakorlat.trim());
-                
+                const updatedParams = new URLSearchParams(
+                  window.location.search
+                );
+                updatedParams.set("gyakorlat_id", values.gyakorlat);
+
                 router.push(`?${updatedParams.toString()}`, { scroll: false });
                 setSelectedGyakorlat(parseInt(values.gyakorlat));
               }
             }, [values.gyakorlat]);
-                
-            
-            
+
             return (
               <>
                 <Form className={styles.form}>
                   <label>
                     <Text variant="caption">Gyakorlat:</Text>
                   </label>
-                  <FormikSelect
-                    name="gyakorlat"
-                    options={gyakorlatsOptions}
-
-                    />
+                  <FormikSelect name="gyakorlat" options={gyakorlatsOptions} />
                 </Form>
 
-                <ResponsiveContainer className={styles.bar} width={"100%"} height={400}>
-                    {items.length === 0 ? (
+                <ResponsiveContainer
+                  className={styles.bar}
+                  width={"100%"}
+                  height={400}
+                >
+                  {items.length === 0 ? (
                     <div className={styles["no-data"]}>
                       <Text variant="h1">Jelenleg nincsenek adatok</Text>
                     </div>
-                    ) : (
+                  ) : (
                     <BarChart
                       width={1100}
                       height={400}
                       data={items}
                       className={styles["bar-chart"]}
                       margin={{
-                      top: 5,
-                      left: 0,
-                      right: 15,
-                      bottom: 5,
+                        top: 5,
+                        left: 0,
+                        right: 15,
+                        bottom: 5,
                       }}
                     >
-                      <XAxis reversed dataKey="date" className={styles["date"]} interval={0} overflow={0} height={46} fontFamily="manrope" angle={30} tickMargin={17} stroke="var(--color-light)" label={<Text variant="caption">ad</Text>} />
+                      <XAxis
+                        reversed
+                        dataKey="date"
+                        className={styles["date"]}
+                        interval={0}
+                        height={46}
+                        fontFamily="manrope"
+                        angle={30}
+                        tickMargin={17}
+                        stroke="var(--color-light)"
+                      />
                       <YAxis stroke="var(--color-light)" />
-                      <Tooltip cursor={{ fill: 'none' }} />
-                      <Bar radius={[5, 5, 0, 0]} dataKey="weight" barSize={"5%"} fill="var(--color-primary-50)" activeBar={<Rectangle fill="var(--color-primary-10)" />} />
+                      <Tooltip cursor={{ fill: "none" }} />
+                      <Bar
+                        radius={[5, 5, 0, 0]}
+                        dataKey="weight"
+                        barSize={"5%"}
+                        fill="var(--color-primary-50)"
+                        activeBar={<Rectangle fill="var(--color-primary-10)" />}
+                      />
                     </BarChart>
-                    )}
+                  )}
                 </ResponsiveContainer>
-
-
               </>
             );
           }}
--- frontend/src/components/client/_filters/StatisztikaFilter/StatisztikaFilter.tsx
+++ frontend/src/components/client/_filters/StatisztikaFilter/StatisztikaFilter.tsx
@@ -1,62 +1,58 @@
 import { Form, Formik } from "formik";
 import { FormikSelect } from "../../_inputs";
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter, useSearchParams } from "next/navigation";
 import styles from "./StatisztikaFilter.module.scss";
 import { Text } from "@/components/server";
+
 interface StatFilterProps {
   onFilterChange: (values: any) => void;
 }
 
-
-
 const StatFilter: React.FC<StatFilterProps> = ({ onFilterChange }) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   const Option = [
     { label: "hét", value: "week" },
     { label: "hónap", value: "month" },
     { label: "félév", value: "halfyear" },
     { label: "összes", value: "all" },
-  ]
+  ];
+
+  const initialIdotartam = searchParams.get("type") || "all";
 
   return (
     <Formik
-      classname={styles.formikInput}
-      initialValues={{ idotartam: "" }}
-      onSubmit={() => { }}
+      className={styles.formikInput}
+      initialValues={{ idotartam: initialIdotartam }}
+      enableReinitialize
+      onSubmit={() => {}}
     >
       {({ setFieldValue, values }) => {
         useEffect(() => {
-          const queryParams = new URLSearchParams();
-          if (values.idotartam) queryParams.set("type", values.idotartam);
-          setFieldValue("idotartam", values.idotartam);
-          onFilterChange(values.idotartam)
+          if (values.idotartam) {
+            const queryParams = new URLSearchParams(window.location.search);
+            queryParams.set("type", values.idotartam);
+            
+          
+            router.push(`?${queryParams.toString()}`, { scroll: false });
 
-
+            onFilterChange(values.idotartam);
+          }
         }, [values.idotartam]);
 
         return (
-          <>
-            <Form style={{ maxWidth: "320px", marginTop: '0.5rem' }} >
+          <Form style={{ maxWidth: "320px", marginTop: "0.5rem" }}>
             <label>
-                    <Text variant="caption">Időtartam:</Text>
-                  </label>
-              <FormikSelect
-
-                name="idotartam"
-              
-                options={Option}
-
-              />
-            </Form>
-
-
-
-
-          </>
+              <Text variant="caption">Időtartam:</Text>
+            </label>
+            <FormikSelect name="idotartam" options={Option} />
+          </Form>
         );
       }}
     </Formik>
   );
+};
 
-}
 export default StatFilter;
