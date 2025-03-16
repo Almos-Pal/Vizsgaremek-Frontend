61be2ed8b0bb377ac4d554739092dc18f861738f
RobozPeter <peti.roboz@gmail.com>
2025-03-16 17:28:33 +0100

    feat(SCRUM-82): select

--- frontend/src/app/edzes/page.tsx
+++ frontend/src/app/edzes/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import useEdzes from '@/hooks/useEdzes';
 import ContentLayout from '@/components/server/Layout/ContentLayout/ContentLayout';
 import EdzesBlock from '@/components/client/EdzesBlock/EdzesBlock';
@@ -14,17 +14,17 @@ import { FormikSelect } from '@/components/client/_inputs';
 
 function EdzesekPage() {
     const { data: session } = useSession();
-    console.log('edzes user session data: ',session?.user.isAdmin);
-    
+    console.log('edzes user session data: ', session?.user.isAdmin);
+
     const router = useRouter();
     const searchParams = useSearchParams();
     const initialPage = parseInt(searchParams.get("page") || "1", 10);
     const [page, setPage] = useState(initialPage);
     const [isModalOpen, setIsModalOpen] = useState(false);
 
-    
-    
-    const { data: edzesek, isLoading, error } = useEdzes.getEdzesek({
+
+
+    const { data: edzesek, isLoading, error, refetch } = useEdzes.getEdzesek({
         page,
         limit: 3,
         user_id: session?.user.user_id
@@ -34,7 +34,7 @@ function EdzesekPage() {
     if (isLoading) return <div>Loading...</div>;
     if (error) return <div>Error loading workouts</div>;
 
-   
+
     const workouts = Array.isArray(edzesek?.items?.[0])
         ? edzesek.items.flat()
         : edzesek?.items;
@@ -49,25 +49,45 @@ function EdzesekPage() {
         const storedStartTime = localStorage.getItem("edzesStartTime");
         storedStartTime ? localStorage.removeItem("edzesStartTime") : null;
     }
+    const Option = [
+        { label: "Kedvencek alapján", value: "byFavorite" },
+        { label: "Dátum alapján növekvő", value: "asc" },
+        { label: "Dátum alapján csökkenő", value: "desc" },
+    ]
 
     return (
+
         <ContentLayout header="Edzések">
-            <Formik onSubmit={() => { }}>
-            
-                    intialValue="desc"
-                <Form >
-                    <FormikSelect {...FormikSelect}>
-                        <option value="byFavorite">Kedvencek alapján</option>
-                        <option value="asc">Dátum alapján növekvő</option>
-                        <option value="desc">Dátum alapján csökkenő</option>
-                    </FormikSelect>
-                </Form>
+            <Formik
+                
+                onSubmit={() => { }}
+                initialValues={{ order: "desc" }}>
+                {({ setFieldValue, values }) => {
+                    useEffect(() => {
+                        const queryParams = new URLSearchParams();
+                        if (values.order) queryParams.set("type", values.order);
+                        setFieldValue("order", values.order);
+                        refetch();
+
+
+                    }, [values.order]);
+                    return (
+                        <Form style={{ maxWidth: "750px",width:"100%", margin: "10px", justifySelf:'center' }} >
+                            <FormikSelect
+                                placeholder='ListaRendezés'
+                                name="order"
+                                options={Option}>
+
+                            </FormikSelect>
+                        </Form>
+                    )
+                }}
             </Formik>
 
             {workouts?.map((edzes: any) => (
                 <EdzesBlock key={edzes.edzes_id} edzes={edzes} />
             ))}
-           
+
             <div className='flex justify-center mt-4 pb-4'>
                 <Button width={225} color='secondary' rightIcon='AddIcon' onClick={(handleNewEdzes)} >Edzés</Button>
             </div>
@@ -77,8 +97,8 @@ function EdzesekPage() {
                 onClose={() => setIsModalOpen(false)}
                 width={350}
                 showCloseButton={false}
-                
-                
+
+
             >
 
                 <NewEdzesForm onSuccess={() => setIsModalOpen(false)} onCancel={() => setIsModalOpen(false)} />
