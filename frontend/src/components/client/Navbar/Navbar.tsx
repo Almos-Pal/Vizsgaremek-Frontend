54d19751ca84a59225f175694617c8e08ac07869
DemendiAkos <akosdemendi@gmail.com>
2025-03-19 11:16:31 +0100

    feat(SCRUM-91): navbar menu closes when clicking outside of it feature fully working, navbar icons turn white when on the same page

--- frontend/src/components/client/Navbar/Navbar.tsx
+++ frontend/src/components/client/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import Button from '../Button/Button';
 import IconButton from '../IconButton/IconButton';
 import { Text } from '@/components/server';
 import { signOut, useSession } from 'next-auth/react';
+import { usePathname } from 'next/navigation';
 
 const Navbar: React.FC = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -13,10 +14,12 @@ const Navbar: React.FC = () => {
     const desktopToggleRef = useRef<HTMLButtonElement>(null);
     const mobileToggleRef = useRef<HTMLButtonElement>(null);
     const { data: session } = useSession();
+    const pathname = usePathname();
+
 
     const toggleMenu = () => {
         setMenuOpen((prevOpen) => !prevOpen);
-        console.log('Menu clicked', menuOpen);
+        console.log('Toggle clicked. Menu open:', !menuOpen);
     };
 
     const handleLogout = () => {
@@ -26,28 +29,24 @@ const Navbar: React.FC = () => {
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
             if (menuOpen) {
-                // If the click is on one of the toggle buttons, do nothing.
                 if (
                     (desktopToggleRef.current && desktopToggleRef.current.contains(event.target as Node)) ||
                     (mobileToggleRef.current && mobileToggleRef.current.contains(event.target as Node))
                 ) {
                     return;
                 }
-                // If the click is outside both menu containers, close the menu.
                 if (
-                    desktopMenuRef.current &&
-                    !desktopMenuRef.current.contains(event.target as Node) &&
-                    mobileMenuRef.current &&
-                    !mobileMenuRef.current.contains(event.target as Node)
+                    (desktopMenuRef.current && !desktopMenuRef.current.contains(event.target as Node)) &&
+                    (mobileMenuRef.current && !mobileMenuRef.current.contains(event.target as Node))
                 ) {
                     setMenuOpen(false);
                 }
             }
         };
 
-        document.addEventListener("mousedown", handleClickOutside);
+        document.addEventListener("click", handleClickOutside);
         return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
+            document.removeEventListener("click", handleClickOutside);
         };
     }, [menuOpen]);
 
@@ -55,16 +54,18 @@ const Navbar: React.FC = () => {
         <>
             {/* Desktop navbar */}
             <nav className={styles.desktopnavbar}>
-                <IconButton icon="HomeIcon" color="transparent" iconProps={{ size: 40 }} href={`/dashboard`} />
-                <IconButton icon="ProfileIcon" color="transparent" href={`/profil/${session?.user.user_id}`} iconProps={{ size: 50 }} />
+                <IconButton icon="HomeIcon" color={pathname === '/dashboard' ? 'secondary' : 'transparent'}
+                    iconProps={{ size: 40 }} href={`/dashboard`} />
+                <IconButton icon="ProfileIcon" color={pathname === `/profil/${session?.user.user_id}` ? 'secondary' : 'transparent'}
+                    href={`/profil/${session?.user.user_id}`} iconProps={{ size: 50 }} />
                 <IconButton
-                    ref={desktopToggleRef}
                     icon="AddIcon"
                     color="secondary"
                     iconProps={{ size: 60 }}
                     onClick={toggleMenu}
                 />
-                <IconButton icon="ChartIcon" color="transparent" iconProps={{ size: 40 }} />
+                <IconButton href={'/statisztika'} icon="ChartIcon" color={pathname === '/statisztika' ? 'secondary' : 'transparent'}
+                    iconProps={{ size: 40 }} />
                 <IconButton
                     icon="LogoutIcon"
                     color="transparent"
@@ -99,20 +100,21 @@ const Navbar: React.FC = () => {
             {/* Mobile navbar */}
             <nav className={styles.mobilenavbar}>
                 <div className={styles.navItem}>
-                    <IconButton icon="HomeIcon" color="transparent" iconProps={{ size: 30 }} />
+                    <IconButton href={'/dashboard'} icon="HomeIcon" color={pathname === '/dashboard' ? 'secondary' : 'transparent'}
+                        iconProps={{ size: 30 }} />
                     <Text variant="caption" color="var(--color-grey-300)">
                         Főoldal
                     </Text>
                 </div>
                 <div className={styles.navItem}>
-                    <IconButton icon="ProfileIcon" href={`/profil/${session?.user.user_id}`} color="transparent" iconProps={{ size: 40 }} />
+                    <IconButton icon="ProfileIcon" href={`/profil/${session?.user.user_id}`} color={pathname === `/profil/${session?.user.user_id}` ? 'secondary' : 'transparent'}
+                        iconProps={{ size: 40 }} />
                     <Text variant="caption" color="var(--color-grey-300)">
                         Profil
                     </Text>
                 </div>
                 <div className={styles.navItem}>
                     <IconButton
-                        ref={mobileToggleRef}
                         icon="AddIcon"
                         color="secondary"
                         iconProps={{ size: 50 }}
@@ -120,7 +122,8 @@ const Navbar: React.FC = () => {
                     />
                 </div>
                 <div className={styles.navItem}>
-                    <IconButton icon="ChartIcon" color="transparent" iconProps={{ size: 30 }} />
+                    <IconButton icon="ChartIcon" color={pathname === '/statisztika' ? 'secondary' : 'transparent'}
+                        iconProps={{ size: 30 }} href={'/statisztika'} />
                     <Text variant="caption" color="var(--color-grey-300)">
                         Statisztika
                     </Text>
