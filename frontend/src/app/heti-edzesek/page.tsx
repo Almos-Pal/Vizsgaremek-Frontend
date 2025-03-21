f920d1c9defbeeec19180c60b591082ecc63fbb3
Almos-Pal <pal.almos04@gmail.com>
2025-03-21 10:37:36 +0100

    fix(SCRUM-95): add back button for heti-edzesek show text if no data avalaible

--- frontend/src/app/heti-edzesek/page.tsx
+++ frontend/src/app/heti-edzesek/page.tsx
@@ -1,23 +1,53 @@
 "use client"
 
-import { EdzesBlock, SubHeader } from "@/components/client"
+import { Button, EdzesBlock, SubHeader } from "@/components/client"
 import ContentLayout from "@/components/server/Layout/ContentLayout/ContentLayout"
 import styles from './page.module.scss'
 import { BodySVG } from "@/components/server"
 import { useSession } from "next-auth/react"
 import { useEdzes } from "@/hooks"
+import { Text } from "@/components/server"
+import { useRouter } from "next/navigation";
+import { Loading } from "@/components/client/Loading/Loading"
+
 
 const WeeklyTrainingPage = () => {
     const { data: session } = useSession();
-    const { data: currentWeekEdzesek } = useEdzes.getCurrentWeekEdzesek( session?.user.user_id! );
+    const { data: currentWeekEdzesek, isLoading } = useEdzes.getCurrentWeekEdzesek( session?.user.user_id! );
+    const router = useRouter();
+
+    if(isLoading) {
+        return (
+            <ContentLayout 
+            header="Heti Edzések" 
+            >   
+            <div className="flex justify-center items-center flex-col ">
+                <Loading  hasParent/>
+            </div>
+            </ContentLayout>
+        )
+    }
+
+    if(currentWeekEdzesek?.edzesek.length === 0) {
+    return (
+        <ContentLayout 
+        header="Heti Edzések" 
+        >   
+        <div className="flex justify-center items-center flex-col gap-4 text-center">
+
+            <Text variant="h5">Jelenleg nincsen edzésed a hétre</Text>
+            <Button additionalClassName={styles.button}color="primary" onClick={()=> router.back()}>Vissza</Button>
+        </div>
+        </ContentLayout>
+    )
+    }
 
     return (
         <ContentLayout 
         header="Heti Edzések" 
         >
-            <div className={styles.subheader}>
 
-            </div>
+         
         <div className="flex justify-center items-center flex-col">
             <div className={styles.container}>
 
@@ -28,10 +58,14 @@ const WeeklyTrainingPage = () => {
 
             <BodySVG  size={330}  selectedMuscleIds={currentWeekEdzesek?.fo_izomcsoportok } secondaryMuscleIds={currentWeekEdzesek?.izomcsoportok} />
             </div>
-        </div>
             {currentWeekEdzesek?.edzesek?.map((edzes: any) => (
                 <EdzesBlock key={edzes.edzes_id} edzes={edzes} />
             ))}
+          
+            <Button additionalClassName={styles.button}color="primary" onClick={()=> router.back()}>Vissza</Button>
+            </div>
+          
+
       
         </ContentLayout>
     )
