74ac9616bc157e364e57ac722e56e4d9feeb5a77
Almos-Pal <pal.almos04@gmail.com>
2025-03-23 13:08:26 +0100

    fix(SCRUM-98): remove build in next js error redirect to dashboard page as default

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,18 @@ const nextConfig: NextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src/")],
   },
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ]
+  },
+
+
 };
 
 export default nextConfig;
